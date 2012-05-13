90d4eb63aa65c5fe13ac3fbdf9b5df884c3c0eb2
Daniel Jackson <danyboyj@gmail.com>
2012-05-13 12:19:57 +0100

    Lookup validation done on slug

--- application/models/Realm.php
+++ application/models/Realm.php
@@ -2,9 +2,10 @@
 class App_Model_Realm extends App_Model_Base {
 	protected $_dbTableName = 'realms';
 
-	public function validateName($name, $region_id) {
+	public function validateName($slug, $region_id) {
 		$db = Zend_Registry::get('db');
-		$query = "SELECT id, name, slug FROM realms WHERE name = " . $db->quote(ucfirst($name)) . " AND region = " . intval($region_id);
+		
+		$query = "SELECT id, name, slug FROM realms WHERE slug = " . $db->quote($slug) . " AND region = " . intval($region_id);
 
 		$result = $db->fetchRow($query);
 
